825d0392328f81bead53b53eb990e42e2d6a05ef
Kent Collins <kent.r.collins@gmail.com>
2016-04-15 06:50:19 +0100

    restored separate from/to endpoints of Road

--- src/roadgraph/Road.java
+++ src/roadgraph/Road.java
@@ -7,14 +7,15 @@ package roadgraph;
 import geography.GeographicPoint;
 
 public class Road {
-	GeographicPoint[] endpoints;
+	GeographicPoint from, to;
 	String roadName;
 	String roadType;
 	double length;
 	
 	public Road(GeographicPoint start, GeographicPoint finish, String roadName,
 			String roadType, double length){
-		endpoints = new GeographicPoint[] {start, finish};
+		from = start;
+		to = finish;
 		this.roadName = roadName;
 		this.roadType = roadType;
 		this.length = length;
@@ -26,13 +27,17 @@ public class Road {
 	 * @return the opposing vertex or null if the parameter is not an end point.
 	 */
 	public GeographicPoint getOtherEnd(GeographicPoint gp) {
-		if (endpoints[0].equals(gp)) return endpoints[1];
-		if (endpoints[1].equals(gp)) return endpoints[0];
+		if (from.equals(gp)) return to;
+		if (to.equals(gp)) return from;
 		return null;
 	}
 
-	public GeographicPoint[] getEndpoints() {
-		return endpoints;
+	public GeographicPoint getFrom(){
+		return from;
+	}
+	
+	public GeographicPoint getTo(){
+		return to;
 	}
 
 	public String getRoadName() {
@@ -49,7 +54,7 @@ public class Road {
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("[Road from: "+endpoints[0]+" to "+endpoints[1]+"]");
+		sb.append("[Road from: "+from+" to "+to+"]");
 		return sb.toString();
 	}
 	
